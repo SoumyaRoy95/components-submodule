09be24944eb978b1b72d039795c11cd0404bc48b
SoumyaRoy95 <soumyoroy3@gmail.com>
2021-08-06 15:52:56 +0530

    List of articles made

--- articleList.js
+++ articleList.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import Container from '@material-ui/core/Container';
+import { Grid } from '@material-ui/core';
+import Article from './article'
+
 
 const articleList = () => {
+    
     return (
         <div>
-            Hello
+            <Container>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Article/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Article/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Article/>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Article/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Article/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Article/>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
